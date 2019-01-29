d325665d47694a51cabdcbfcbab9bd32081868e3
Ipung Purwono <ipungofficial@gmail.com>
2019-01-29 08:15:08 +0700

    CRUD Sqlite

--- app/src/main/java/com/banyucenter/atlasapp/SqliteHelper.java
+++ app/src/main/java/com/banyucenter/atlasapp/SqliteHelper.java
@@ -14,7 +14,7 @@ public class SqliteHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "atlasgalon";
 
     //DATABASE VERSION
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
     //TABLE NAME
     public static final String TABLE_USERS = "users";
